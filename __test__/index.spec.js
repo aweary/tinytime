c956c027f3b98f99fe5a0b3ce3afd2dec81e7cf6
bjarneo <bjarne.overli@vg.no>
2017-04-29 18:21:18 +0200

    add tests

--- __test__/index.spec.js
+++ __test__/index.spec.js
@@ -1,11 +1,5 @@
 import tinytime from '../src'
 
-// My birthday!
-const date = new Date('September 24, 1992 021:07:30');
-
-// Helper function to render template with the same date.
-const render = template => tinytime(template).render(date)
-
 describe('tinytime', () => {
   it('should export a function', () => {
     expect(typeof tinytime).toBe('function')
@@ -15,6 +9,12 @@ describe('tinytime', () => {
     expect(typeof template).toBe('object')
   });
   describe('rendering', () => {
+    // My birthday!
+    const date = new Date('September 24, 1992 021:07:30');
+
+    // Helper function to render template with the same date.
+    const render = template => tinytime(template).render(date)
+
     it('should let you render with a date/time', () => {
       expect(render('{h}:{mm}:{ss}{a} on a {dddd}.')).toEqual('9:07:30PM on a Thursday.');
     });
@@ -67,6 +67,41 @@ describe('tinytime', () => {
       )).toEqual(
         'It was 9:07:30PM on September 24th, 1992.'
       )
+    });
+  });
+
+  describe('rendering with custom language', () => {
+    // Someone elses birthday
+    const date = new Date('October 5, 1987 01:00:00');
+
+    const render = template => tinytime(template, {
+      padMonth: true,
+      padDays: true,
+      padHours: true,
+      localization: {
+        months: ['januar', 'februar', 'mars', 'april', 'mai', 'juni', 'juli', 'august', 'september', 'oktober', 'november', 'desember'],
+        days: ['mandag', 'tirsdag', 'onsdag', 'torsdag', 'fredag', 'lørdag', 'søndag'],
+      }
+    }).render(date);
+
+    it('should let you render with a date/time', () => {
+      expect(render('{h}.{mm}.{ss} på en {dddd}.')).toEqual('01.00.00 på en mandag.');
+    });
+
+    it('full months', () => {
+      expect(render('{MMMM}')).toEqual('oktober');
+    });
+
+    it('days of the week', () => {
+      expect(render('{dddd}')).toEqual('mandag');
+    });
+
+    it('user text with subsitutions', () => {
+      expect(render(
+        'Det var {h}.{mm}.{ss} den {DD} {MMMM}, {YYYY}.'
+      )).toEqual(
+        'Det var 01.00.00 den 05 oktober, 1987.'
+      );
     });
   });
 });
